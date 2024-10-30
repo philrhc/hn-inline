b22c03082c20c25c2607e4ab37c38dcb7be66650
Phil Cummins <philip.cummins@bsc.es>
2024-10-30 09:08:49 +0100

    highlight looks like a link

--- highlight.js
+++ highlight.js
@@ -10,8 +10,7 @@
           let link = linkToComment(comment.objectID)
           let quote = findQuote(text);
           let commentOnQuote = findComment(text);
-          let timeSinceText = timeSince(comment.created_at_i*1000);
-          highlight(quote, author, commentOnQuote, timeSinceText, link);
+          highlight(quote, author, commentOnQuote, link);
         }
       }
 
@@ -22,46 +21,6 @@
       }
     });
 })();
-
-function timeSince(time) { // from https://stackoverflow.com/a/12475270
-    var time_formats = [
-      [60, 'seconds', 1], // 60
-      [120, '1 minute ago', '1 minute from now'], // 60*2
-      [3600, 'minutes', 60], // 60*60, 60
-      [7200, '1 hour ago', '1 hour from now'], // 60*60*2
-      [86400, 'hours', 3600], // 60*60*24, 60*60
-      [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
-      [604800, 'days', 86400], // 60*60*24*7, 60*60*24
-      [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
-      [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
-      [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
-      [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-      [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
-      [2903040000, 'years', 29030400] // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-    ];
-    var seconds = (+new Date() - time) / 1000,
-      token = 'ago',
-      list_choice = 1;
-  
-    if (seconds == 0) {
-      return 'Just now'
-    }
-    if (seconds < 0) {
-      seconds = Math.abs(seconds);
-      token = 'from now';
-      list_choice = 2;
-    }
-    var i = 0,
-      format;
-    while (format = time_formats[i++])
-      if (seconds < format[0]) {
-        if (typeof format[2] == 'string')
-          return format[list_choice];
-        else
-          return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
-      }
-    return time;
-  }
 
 function linkToComment(commentId) {
   return "https://news.ycombinator.com/item?id=" + commentId
@@ -116,7 +75,7 @@ function matchElement(quote) {
   } 
 }
 
-function highlight(quote, author, comment, timeSinceText, link) {
+function highlight(quote, author, comment, link) {
   console.log(quote);
 
   const matchingElement = matchElement(quote);
@@ -124,22 +83,21 @@ function highlight(quote, author, comment, timeSinceText, link) {
     console.log("Did not find: " + quote);
     return;
   }
+
   let allText = matchingElement.textContent;
   let textBefore = allText.split(quote)[0];
   let textAfter = allText.split(quote)[1];
   
   const highlightDiv = divWithClassName("hn-inline-highlight");
+  highlightDiv.addEventListener("click", function() {
+    window.location.href = link;
+  });
   const quotedTextNode = document.createTextNode(quote);
   const extensionDiv = divWithClassName("hn-inline-extension");
   const extensionHeaderDiv = divWithClassName("hn-inline-extension-header");
   const authorA = document.createElement("a");
-  authorA.href = `https://news.ycombinator.com/user?id=${author}`
   authorA.innerText = author;
-  const commentLinkA = document.createElement("a");
-  author.href = link;
-  author.innerText = timeSinceText;
   extensionHeaderDiv.appendChild(authorA);
-  extensionHeaderDiv.appendChild(commentLinkA);
   const commentDiv = divWithClassName("hn-inline-comment")
   addText(commentDiv, comment);
   extensionDiv.appendChild(extensionHeaderDiv);
